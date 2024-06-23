fb6c82c03a427bb394f49cca393200c0264abed9
khantyeyint <khantyeyint13@gmail.com>
2024-06-23 08:37:40 +0630

    make page dynamic

--- app/page.js
+++ app/page.js
@@ -1,26 +1,8 @@
+export const dynamic = 'force-dynamic';
+
 import ColorPalette from '@/components/color-palette';
 
 const HomePage = async () => {
-  // const fetchData = async (input) => {
-  //   const res = await fetch(
-  //     'http://colormind.io/api/',
-  //     {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         model: 'default',
-  //         input: input,
-  //       }),
-  //     },
-  //     { cache: 'no-store' }
-  //   );
-
-  //   const { result } = await res.json();
-
-  //   return result;
-  // };
-
-  // console.log(await fetchData(['N', 'N', 'N', 'N', 'N']));
-
   return (
     <div className="w-full ">
       <ColorPalette />
