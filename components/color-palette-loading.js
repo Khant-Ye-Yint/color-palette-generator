39481e9b0836e66a0707dd4f1f369c95c2d21e6a
khantyeyint <khantyeyint13@gmail.com>
2024-06-25 14:39:02 +0630

    loading style changed

--- components/color-palette-loading.js
+++ components/color-palette-loading.js
@@ -1,5 +1,5 @@
 const Block = () => (
-  <div className="p-3 space-y-3 dark:bg-slate-700 bg-neutral-200">
+  <div className="p-3 space-y-3 rounded-md">
     <div className="rounded-sm h-44 dark:bg-slate-500 bg-neutral-100 animate-pulse"></div>
     <div className="flex items-center justify-between">
       <div className="w-20 h-6 rounded-sm dark:bg-slate-500 bg-neutral-100 animate-pulse"></div>
@@ -13,7 +13,7 @@ const Block = () => (
 
 const ColorPaletteLoading = () => {
   return (
-    <div className="mt-10 overflow-hidden text-gray-800 rounded-lg dark:text-white bg-slate-100 dark:bg-neutral-900">
+    <div className="mt-10 overflow-hidden text-gray-800 rounded-lg dark:text-white ">
       <div className="grid grid-cols-5">
         {[0, 0, 0, 0, 0].map((item, index) => (
           <Block key={index} />
